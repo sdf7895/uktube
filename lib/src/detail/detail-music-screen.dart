cbe9c408e73ade9b597cba1989829b87220338a7
sdf7895@naver.com <sdf7895@naver.com>
2023-11-05 23:13:57 +0900

    chore : 상세 뮤직 화면 클릭시 상태를 콜백함수로 전달하는 로직추가

--- lib/src/detail/detail-music-screen.dart
+++ lib/src/detail/detail-music-screen.dart
@@ -4,8 +4,14 @@ import 'package:youtube_music_clone_coding/src/widget/widget-image/image.dart';
 import 'package:youtube_music_clone_coding/src/widget/widget-music-player/music-player.dart';
 import 'package:youtube_music_clone_coding/src/widget/widget-music-progress/music-progress.dart';
 
+// ignore: must_be_immutable
 class MusicScreen extends StatefulWidget {
-  const MusicScreen({super.key});
+  Function(bool status) onItemClick;
+
+  MusicScreen({
+    super.key,
+    required this.onItemClick,
+  });
 
   @override
   State<MusicScreen> createState() => _MusicScreenState();
@@ -19,23 +25,62 @@ class _MusicScreenState extends State<MusicScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         mainAxisSize: MainAxisSize.min,
         children: [
+          MusicHeader(
+            onItemClick: (status) {
+              widget.onItemClick(status);
+            },
+          ),
           MusicImage(
             width: 400,
             height: 400,
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Text(
-              '테스트 제목',
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30,
-                  color: Colors.white),
-            ),
-          ),
-          MusicProgressBar(),
+          const MusicText(),
+          const MusicProgressBar(),
           MusicPlayer(),
         ],
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class MusicHeader extends StatelessWidget {
+  Function(bool status) onItemClick;
+  MusicHeader({
+    super.key,
+    required this.onItemClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            onItemClick(false);
+          },
+          icon: Icon(
+            Icons.keyboard_arrow_down,
+            color: Colors.white,
+            size: 30,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MusicText extends StatelessWidget {
+  const MusicText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: const Text(
+        '테스트 제목',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white),
       ),
     );
   }
