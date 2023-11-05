7ce048c94937038ab21d1d379045f0e483647d36
sdf7895@naver.com <sdf7895@naver.com>
2023-11-05 23:13:10 +0900

    chore : 하단 유튜브 미니 플레이어

---
+++ lib/src/widget/widget-music-player/music-mini-player.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+import '../widget-container/ratio-container.dart';
+
+class MusicMiniPlayer extends StatefulWidget {
+  final bool isOpen;
+  MusicMiniPlayer({
+    super.key,
+    this.isOpen = true,
+  });
+
+  @override
+  State<MusicMiniPlayer> createState() => _MusicMiniPlayerState();
+}
+
+class _MusicMiniPlayerState extends State<MusicMiniPlayer>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _offsetAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return SlideTransition(
+          position: _offsetAnimation,
+          child: RatioContainer(
+            ratioHeight: 0.06,
+            child: Row(
+              children: [
+                Image.network(
+                  'https://source.unsplash.com/random/50×50',
+                  fit: BoxFit.cover,
+                  width: 50,
+                  height: 50,
+                ),
+                _singerInfo(),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.play_arrow,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.replay,
+                    color: Colors.white,
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.cancel_sharp,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    )..forward();
+
+    _offsetAnimation = Tween<Offset>(
+      begin: const Offset(12.0, 12.0),
+      end: const Offset(0.0, 12.0),
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.fastOutSlowIn,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(MusicMiniPlayer oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.isOpen) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+  }
+
+  Widget _singerInfo() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            '테스트노래제목',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Text(
+            '테스트가수',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
+  }
+}
